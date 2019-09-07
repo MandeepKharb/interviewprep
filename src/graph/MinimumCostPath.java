d47f186895bf5bfd414104eee38665e30d094676
Mandeep <mandesi@adobe.com>
2019-09-07 09:40:57 +0530

    src folder added

---
+++ src/graph/MinimumCostPath.java
@@ -0,0 +1,35 @@
+package graph;
+
+/*
+*Given a square grid of size N, each cell of which contains integer cost which represents a cost to traverse through that cell, we need to find a path from top left cell to bottom right cell by which total cost incurred is minimum.
+
+Note : It is assumed that negative cost cycles do not exist in input matrix.
+
+Input:
+The first line of input will contain number of testcases T. Then T test cases follow. Each test case contains 2 lines. The first line of each test case contains an integer N denoting the size of the grid. Next line of each test contains a single line containing N*N space separated integers depicting the cost of respective cell from (0,0) to (N,N).
+
+Output:
+For each test case output a single integer depecting the minimum cost to reach the destination.
+
+Constraints:
+1 <= T <= 50
+1 <= N <= 50
+
+Example:
+Input:
+2
+5
+31 100 65 12 18 10 13 47 157 6 100 113 174 11 33 88 124 41 20 140 99 32 111 41 20
+2
+42 93 7 14
+
+Output:
+327
+63
+*
+* */
+
+public class MinimumCostPath {
+
+    
+}
